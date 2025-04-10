5bf492405cd9d442af50b7e6459ab79e34feae13
Nahuel Soria Parodi <nahuelsoriap@gmail.com>
2025-04-10 18:53:17 -0300

    Update useImageCompression.jsx

--- useImageCompression.jsx
+++ useImageCompression.jsx
@@ -15,14 +15,10 @@ export const useImageCompression = () => {
 
     try {
       const originalSizeKB = file.size / 1024
-      console.log('Tamaño original:', originalSizeKB.toFixed(2), 'KB')
 
       const compressedFile = await imageCompression(file, options)
       const compressedSizeKB = compressedFile.size / 1024
       const porcentajeReducido = ((1 - compressedFile.size / file.size) * 100).toFixed(2)
-
-      console.log('Tamaño comprimido:', compressedSizeKB.toFixed(2), 'KB')
-      console.log(`Reducción: ${porcentajeReducido}%`)
 
       return compressedFile
     } catch (error) {
